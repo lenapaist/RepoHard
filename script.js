528fa982c12dfef409e038368bba3bda0c1d7ca0
Elena Shilova <lena.paist2000@gmail.com>
2021-11-30 22:40:17 +0300

    выполнила доп урок 4

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ let lang = prompt("Выберите язык:"),
     ru = "Пн, Вт, Ср, Чт, Пт, Сб, Вс.",
     en = "Mon, Tue, Wed, Thu, Fri, Sat, Sun.",
     langArray = [],
-    namePerson = prompt("Введите имя");
+    namePerson = prompt("Введите имя"),
+    enterString = prompt("Введите произвольные символы");
 
 if (lang === "ru") {
     console.log(ru);
@@ -39,10 +40,23 @@ switch (lang) {
 
 langArray["ru"] = ["Пн", "Вт", "СР", "Чт", "Пт", "Сб", "Вс"];
 langArray["en"] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-console.log(langArray[lang].join());
+
+function checkStringAndLength(str) {
+    if (typeof str !== "string") {
+        return "Не строка!";
+    } else if (str.length > 30) {
+        return str.substring(0, 30) + "...";
+    } else {
+        return str.trim();
+    }
+}
 
 namePerson === "Артем"
     ? console.log("Директор")
     : namePerson === "Александр"
     ? console.log("Преподаватель")
     : console.log("Студент");
+
+console.log(checkStringAndLength(enterString));
+
+console.log(langArray[lang].join());
