1519a52ca2dd52442f2981cfecf55531fa71f147
Elena Shilova <lena.paist2000@gmail.com>
2021-12-09 01:25:29 +0300

    выполнила доп задание урок 9

--- script.js
+++ script.js
@@ -63,32 +63,100 @@
 
 // HARD LESSON05
 // Task 1
-let arr = [
-    "234234",
-    "12323435",
-    "4567567",
-    "45676756",
-    "56543457",
-    "976534567",
-    "25754865",
-];
+// let arr = [
+//     "234234",
+//     "12323435",
+//     "4567567",
+//     "45676756",
+//     "56543457",
+//     "976534567",
+//     "25754865",
+// ];
 
-function getNumbersStartingWithTheSpecified(num1, num2) {
-    let result = arr.filter(function (item) {
-        if (
-            Number(item.split("")[0]) === num1 ||
-            Number(item.split("")[0]) === num2
-        ) {
-            return Number(item);
-        }
-    });
-    return result;
-}
-console.log(getNumbersStartingWithTheSpecified(2, 4));
-// Task 2
-firstСycle: for (let i = 2; i <= 101; i++) {
-    for (let j = 2; j < i; j++) {
-        if (i % j == 0) continue firstСycle;
+// function getNumbersStartingWithTheSpecified(num1, num2) {
+//     let result = arr.filter(function (item) {
+//         if (
+//             Number(item.split("")[0]) === num1 ||
+//             Number(item.split("")[0]) === num2
+//         ) {
+//             return Number(item);
+//         }
+//     });
+//     return result;
+// }
+// console.log(getNumbersStartingWithTheSpecified(2, 4));
+// // Task 2
+// firstСycle: for (let i = 2; i <= 101; i++) {
+//     for (let j = 2; j < i; j++) {
+//         if (i % j == 0) continue firstСycle;
+//     }
+//     console.log(i);
+// }
+// HARD LESSON09
+const dateTypeA = document.querySelector(".date-type-a"),
+    dateTypeB = document.querySelector(".date-type-b"),
+    wordsHours = ["час", "часов", "часа"],
+    wordsMinutes = ["минута", "минут", "минуты"],
+    wordsSeconds = ["секунда", "секунд", "секунды"];
+
+function declensions(int, words) {
+    int = Math.abs(int) % 100;
+    let int1 = int % 10;
+    if (int > 10 && int < 20) {
+        return words[1];
     }
-    console.log(i);
+    if (int1 > 1 && int1 < 5) {
+        return words[2];
+    }
+    if (int1 == 1) {
+        return words[0];
+    }
+    return words[1];
 }
+
+function numberFormat(int) {
+    if (int < 10) {
+        return `0${int}`;
+    }
+    return int;
+}
+
+function getDateTypeA() {
+    let currentDate = new Date(),
+        generalInfoDateTypeA = currentDate.toLocaleDateString("ru-US", {
+            weekday: "long",
+            month: "long",
+            day: "numeric",
+        }),
+        year = currentDate.getFullYear(),
+        hours = currentDate.getHours(),
+        minutes = currentDate.getMinutes(),
+        seconds = currentDate.getSeconds();
+    dateTypeA.innerHTML = `Сегодня ${generalInfoDateTypeA} ${year} года ${hours} ${declensions(
+        hours,
+        wordsHours,
+    )} ${minutes} ${declensions(
+        minutes,
+        wordsMinutes,
+    )} ${seconds} ${declensions(seconds, wordsSeconds)}`;
+}
+
+function getDateTypeB() {
+    let currentDate = new Date(),
+        year = currentDate.getFullYear(),
+        month = currentDate.getMonth(),
+        day = currentDate.getDay(),
+        hours = currentDate.getHours(),
+        minutes = currentDate.getMinutes(),
+        seconds = currentDate.getSeconds();
+
+    dateTypeB.innerHTML = `${numberFormat(day)}.${numberFormat(
+        month,
+    )}.${numberFormat(year)} - ${numberFormat(hours)}:${numberFormat(
+        minutes,
+    )}:${numberFormat(seconds)}`;
+}
+setInterval(function () {
+    getDateTypeA();
+    getDateTypeB();
+}, 1000);
